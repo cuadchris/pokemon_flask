6dae6efe6fbc370df00c8b8a0cb0636ee65d4dc7
Christopher <cuadradochris@hotmail.com>
2022-10-20 12:32:24 -0400

    holy hell. hope nothing broke

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,55 @@
-from flask import render_template, redirect, url_for
-from app import app
-from app.forms import GetPoke
+from flask import render_template, redirect, url_for, flash, request
+from app import app, db
+from app.forms import GetPoke, LoginForm, RegistrationForm
 from app.getPokemon import *
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title = 'Home')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect('pokemon')
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data)
+        user.set_password(form.password.data)
+        with app.app_context():
+            db.session.add(user)
+            db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('signup.html', title='Sign up', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 @app.route('/pokemon', methods = ['GET', 'POST'])
+@login_required
 def pokemon():
     form = GetPoke()
     pokemon = ""
--- config.py
+++ config.py
@@ -2,3 +2,7 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    #     'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = "postgresql://uqytcpjw:MJcLqItXtfgIT-NGGFwNSrrDpBXWIhAk@peanut.db.elephantsql.com/uqytcpjw"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
