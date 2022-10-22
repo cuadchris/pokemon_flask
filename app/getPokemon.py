ed11631003dd9aba6779dae5419c4c85342d453e
Christopher <cuadradochris@hotmail.com>
2022-10-22 17:46:50 -0400

    nice changes

--- app/getPokemon.py
+++ app/getPokemon.py
@@ -1,5 +1,12 @@
 import requests as r
 
+def getBio(pokemon_id):
+    url = f'https://pokeapi.co/api/v2/pokemon-species/{pokemon_id}/'
+    response = r.get(url)
+    data = response.json()
+    bio = data['flavor_text_entries'][0]['flavor_text']
+    return bio
+
 def addPokemon(pokemon):
         url = f'https://pokeapi.co/api/v2/pokemon/{pokemon.lower()}'
         response = r.get(url)
@@ -11,6 +18,7 @@ def addPokemon(pokemon):
             'name': data['name'],
             'abilities': data['abilities'][0]['ability']['name'],
             'f_shiny': data['sprites']['other']['official-artwork']['front_default'],
-            'stats': [{x['stat']['name']: x['base_stat']} for x in data['stats'][:3]]
+            'stats': [{x['stat']['name']: x['base_stat']} for x in data['stats'][:3]],
+            'bio': getBio(data['id'])
         }
         return pokedict
