d110efe8bc65c2b9ddf6570fe9ddf1f48b1fb6d9
Christopher <cuadradochris@hotmail.com>
2022-10-24 00:59:11 -0400

    added profile editing

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
+from hashlib import md5
 
 db = SQLAlchemy()
 
@@ -12,6 +13,7 @@ class User(UserMixin, db.Model):
     last_name = db.Column(db.String(64), index=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -21,6 +23,11 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
 class UserPokemon(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- app/user/routes.py
+++ app/user/routes.py
@@ -1,10 +1,35 @@
-from flask import Blueprint, render_template
-from app.models import User
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from app.models import User, UserPokemon
+from flask_login import current_user, login_required
+from app.user.forms import EditProfileForm
+from app.models import db
+from datetime import datetime
 
-user = Blueprint('user', __name__, template_folder='user_templates')
+user = Blueprint('user', __name__, template_folder='user_templates', url_prefix = "/user")
+
+@user.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 @user.route('/<username>')
+@login_required
 def showUser(username):
     user = User.query.filter_by(username=username).first_or_404()
+    owned = [x.pokemon_name for x in UserPokemon.query.filter_by(user_id = user.id)]
+    return render_template('user.html', user=user, owned=owned)
 
-    return render_template('user.html', user=user)
+@user.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('user.edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
