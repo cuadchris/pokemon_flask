dbf74883f575392b87633a1c9f17463c61218466
Christopher <cuadradochris@hotmail.com>
2022-10-27 11:24:08 -0400

    changed homepage. added logo

--- app/routes.py
+++ app/routes.py
@@ -34,7 +34,7 @@ def login():
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
-        return redirect('pokemon')
+        return redirect('index')
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/signup', methods=['GET', 'POST'])
